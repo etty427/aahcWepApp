61b7db5279db1a401df289e12033915bb5131f89
etty427 <etty427@aim.com>
2018-11-26 17:24:30 -0500

    added requirements

--- aahc_website/urls.py
+++ aahc_website/urls.py
@@ -20,5 +20,6 @@ from main_site import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('application/', views.application, name='application')
 ]
--- main_site/views.py
+++ main_site/views.py
@@ -9,6 +9,13 @@ def index(request):
     context = {
         
     }
-
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'home.html',context)
+
+def application(request):
+    
+    context = {
+        
+    }
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'application.html',context)
