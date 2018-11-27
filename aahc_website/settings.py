8ab60fc64d2e561c278f96e23a7fb71ba8a147db
etty427 <etty427@aim.com>
2018-11-27 02:18:08 -0500

    added login and sign up pages

--- aahc_website/settings.py
+++ aahc_website/settings.py
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,6 +133,6 @@ STATICFILES_DIRS = (
 )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 # Activate Django-Heroku.
 django_heroku.settings(locals())
--- main_site/urls.py
+++ main_site/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
+from django.conf.urls import include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
- #   path('application/', views.application, name='application')
+    url('', views.index, name='index'),
+    url(r'^aahc_website/', include('aahc_website.urls')),
+    url(r'^main_site/register/',
+        RegistrationView.as_view(success_url='/profile/'),
+        name='django_registration_register'),
+    url(r'^main_site/', include('django_registration.backends.one_step.urls')),
+    url(r'^main_site/', include('django.contrib.auth.urls')),
 ]
